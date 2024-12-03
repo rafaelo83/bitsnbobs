4a06589bfa331396d2b33b1f2252f58ca29720bf
rafaelo83 <jankovicrafael2@gmail.com>
2024-12-03 08:51:30 +0100

    added BitsNBobsClient Java Class and changed Mod description etc.

---
+++ src/main/java/de/rafaelo_/bitsnbobs/BitsNBobsClient.java
@@ -0,0 +1,13 @@
+package de.rafaelo_.bitsnbobs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BitsNBobsClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
